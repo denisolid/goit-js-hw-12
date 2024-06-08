5de6f3cf4e04dabdf4e35ffd83c417225d0bad8c
denisolid <dkolisnichienko321@gmail.com>
2024-06-08 23:23:46 +0000

    v1.2

--- src/main.js
+++ src/main.js
@@ -1,99 +1,83 @@
 import { pixabayApi } from './js/pixabay-api';
-import { imagesRender } from './js/render-functions';
-import { imgBoxLight } from './js/render-functions';
-
+import { imagesRender, imgBoxLight } from './js/render-functions';
 import axios from 'axios';
-
 import iziToast from 'izitoast';
-
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.find-form');
 const gallery = document.querySelector('.gallery-container');
-const loader = document.querySelector('.loader');
 const loadMore = document.querySelector('.btn');
 const loaderPlace = document.querySelector('#load');
-
-
-loadMore.addEventListener('click', onLoadMore);
+const submitBtn = document.querySelector('.find-btn')
 
 let page = 1;
 let findText = '';
-form.addEventListener('submit', async event => {
+
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
+    page = 1;
     gallery.innerHTML = '';
-    const formData = new FormData(form);
-    const findText = formData.get('find-text');
+    findText = new FormData(form).get('find-text');
 
-    if (findText === '') {
-        return;
-    }
+    if (!findText) return;
 
-    gallery.innerHTML = `<div class = "loading">Loading...  <span class="loader"></span></div>`;
-
+    gallery.innerHTML = `<div class="loader"></div>`;
     try {
         const data = await pixabayApi(findText, page);
         if (data.hits.length === 0) {
             iziToast.error({
                 position: 'topRight',
-                message:
-                    'Sorry, there are no images matching your search query. Please try again!',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
             });
             gallery.innerHTML = '';
+            submitBtn.disabled = true;
             return;
         }
-
-        const markup = imagesRender(data.hits);
-        gallery.innerHTML = markup;
+        gallery.innerHTML = imagesRender(data.hits);
         imgBoxLight();
-    } catch (error) {
+    } catch {
         iziToast.error({
             position: 'topRight',
-            message: 'Error',
+            message: 'Ooops',
         });
     } finally {
-        event.target.reset();
+        event.target.reset()
         loadMore.classList.remove('hidden');
+        loadMore.style.display = 'block';
     }
 });
+
+loadMore.addEventListener('click', onLoadMore);
 
 async function onLoadMore() {
     loadMore.classList.add('hidden');
     page += 1;
+    loaderPlace.innerHTML = `<div class="loading">Loading...  <span class="loader"></span></div>`;
 
-    loaderPlace.innerHTML = `<div class = "loading">Loading...  <span class="loader"></span></div>`;
     try {
         const data = await pixabayApi(findText, page);
         if (data.hits.length === 0) {
             loadMore.classList.add('hidden');
-
             iziToast.info({
                 position: 'topRight',
-                message:
-                    'Sorry, there are no images matching your search query. Please try again!',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+            });
+        } else {
+            gallery.insertAdjacentHTML('beforeend', imagesRender(data.hits));
+            imgBoxLight();
+            window.scrollBy({
+                top: 720,
+                behavior: 'smooth',
             });
         }
-
-        loaderPlace.innerHTML = '';
-        loadMore.classList.add('hidden');
-        loadMore.disable = true;
-        const markup = imagesRender(data.hits);
-        gallery.insertAdjacentHTML('beforeend', markup);
-
-        loadMore.disable = true;
-        imgBoxLight();
-        window.scrollBy({
-            top: 720,
-            behavior: 'smooth',
-        });
-
-
-    } catch (error) {
+    } catch {
         iziToast.error({
             position: 'topRight',
-            message: error.message,
+            message: "We're sorry, but you've reached the end of search results.",
         });
+        loadMore.style.display = 'none';
     } finally {
-        loadMore.classList.remove('hidden')
+        loaderPlace.innerHTML = '';
+        loadMore.classList.remove('hidden');
     }
 }
